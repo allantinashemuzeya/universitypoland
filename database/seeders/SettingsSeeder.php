0000e7914f26fb55e1d292325fb43cf8161ea95a
amuzeya <amuzeya@cepr.org>
2025-09-23 10:17:23 +0200

    Add SettingsSeeder for fee settings

---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Setting;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Application fee settings
+        Setting::updateOrCreate(
+            ['key' => 'application_fee_amount'],
+            [
+                'value' => '5000', // €50 in cents
+                'type' => 'integer',
+                'group' => 'fees',
+                'description' => 'Application fee amount in cents'
+            ]
+        );
+
+        // Commitment fee settings
+        Setting::updateOrCreate(
+            ['key' => 'commitment_fee_amount'],
+            [
+                'value' => '35000', // €350 in cents
+                'type' => 'integer',
+                'group' => 'fees',
+                'description' => 'Commitment fee amount in cents'
+            ]
+        );
+    }
+}
